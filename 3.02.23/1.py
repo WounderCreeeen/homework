e894aa41c095165ca78a934d7793b3ecc3df9994
woundercringe <drk22808@gmail.com>
2023-01-29 19:30:23 +0300

    this is my homework :3

---
+++ 3.02.23/1.py
@@ -0,0 +1,7 @@
+a=int(input())
+if a>0:
+    print('number is positive')
+elif a==0:
+    print('number is equal to zero')
+elif a<0:
+    print('number is negative')
---
+++ 3.02.23/5.py
@@ -0,0 +1,6 @@
+a=int(input())
+b=int(input())
+c=[]
+c.append(a)
+c.append(b)
+print(c[1])
